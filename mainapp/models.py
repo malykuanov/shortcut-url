93dd366a85f7a5951185a2e405f7276bfaf2ea5d
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-25 19:15:15 +0300

    Add model for urls

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Urls(models.Model):
+    long_url = models.URLField(max_length=2000)
+    short_url = models.CharField(max_length=5)
+    clicks = models.IntegerField(default=0)
+    time_create = models.DateTimeField(auto_now_add=True)
