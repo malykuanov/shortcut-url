b693bee59bd7aadc787f7c6ba74c6b7df09cafec
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-28 22:05:48 +0300

    Add path for redirect on site from short link

--- shortcut/urls.py
+++ shortcut/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from mainapp.views import index, shorturl
+from mainapp.views import index, shorturl, redirect_on_site
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='home'),
-    path('shorturl/', shorturl, name='shorturl')
+    path('shorturl/', shorturl, name='shorturl'),
+    path('<slug:short_url>/', redirect_on_site, name='redirect_on_site'),
 ]
