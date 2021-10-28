4fa1fba1c433934cc4f09bfb43ae2bafd2ff7964
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-28 21:36:33 +0300

    Add path for short url

--- shortcut/urls.py
+++ shortcut/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from mainapp.views import index
+from mainapp.views import index, shorturl
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='home'),
+    path('shorturl/', shorturl, name='shorturl')
 ]
