c97f17aab938c03d7857456bc032b56f52bd00a8
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-29 21:04:12 +0300

    Added view for checking short url clicks

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponseRedirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import redirect, render
 
-from mainapp.forms import ShortUrlForm
+import re
+
+from mainapp.forms import ShortUrlForm, CheckClickUrlForm
 from mainapp.models import Urls
 
 
@@ -19,6 +22,7 @@ def index(request):
 def shorturl(request):
     short_url = request.session.get('short_url')
     url = Urls.objects.filter(short_url=short_url).first()
+
     return render(request, 'mainapp/shorturl.html', {'url': url})
 
 
@@ -28,3 +32,23 @@ def redirect_on_site(request, short_url):
         return HttpResponseRedirect(url.long_url)
     else:
         return redirect('home')
+
+
+def check_clicks(request):
+    form = CheckClickUrlForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            url = form.cleaned_data['short_url']
+            domain = str(get_current_site(request))
+            regex = r'^' + domain + r'.\w{5}$'
+            if re.match(regex, url):
+                short_url = url.split('/')[1]
+                return redirect('clicks', short_url)
+            else:
+                form.add_error(
+                    None,
+                    "Ссылка указана неверно. Проверьте домен и короткий URL"
+                )
+
+    return render(request, 'mainapp/click_counter.html', {'form': form})
+
