48ae7159b12448ca666d1dbe5f4a480212c4fbee
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-29 21:00:48 +0300

    Add paths for check and show clicks on short_url

--- shortcut/urls.py
+++ shortcut/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
-from mainapp.views import index, shorturl, redirect_on_site
+from mainapp.views import index, shorturl, redirect_on_site, check_clicks, \
+    clicks_counter
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='home'),
     path('shorturl/', shorturl, name='shorturl'),
+    path('url-click-counter/', check_clicks, name='check_clicks'),
+    path('url-click-counter/<slug:short_url>', clicks_counter, name='clicks'),
     path('<slug:short_url>/', redirect_on_site, name='redirect_on_site'),
 ]
