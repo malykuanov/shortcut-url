e53994c7b26c566f5c9b25be48c1bacebd6b573b
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-30 13:34:47 +0300

    Add path for reporting wrong url

--- shortcut/urls.py
+++ shortcut/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
-from mainapp.views import index, shorturl, redirect_on_site, check_clicks, \
-    clicks_counter
+from mainapp.views import (index, shorturl, redirect_on_site, check_clicks,
+                           clicks_counter, report)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +10,6 @@ urlpatterns = [
     path('shorturl/', shorturl, name='shorturl'),
     path('url-click-counter/', check_clicks, name='check_clicks'),
     path('url-click-counter/<slug:short_url>', clicks_counter, name='clicks'),
+    path('report-wrong-url/', report, name='report'),
     path('<slug:short_url>/', redirect_on_site, name='redirect_on_site'),
 ]
