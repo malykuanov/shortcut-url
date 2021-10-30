16cce6f3c23e2df006d3cb7f074defa43943cd77
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-30 13:32:36 +0300

    Add settings for sending by email

--- shortcut/settings.py
+++ shortcut/settings.py
@@ -97,3 +97,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ADMIN_MAIL = env('ADMIN_MAIL')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = env('ADMIN_MAIL')
+EMAIL_HOST_PASSWORD = env('ADMIN_MAIL_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
