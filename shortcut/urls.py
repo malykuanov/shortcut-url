3b1c4517e68336027abd373be36d9b28f370b32d
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-30 14:25:57 +0300

    Add path for contact with team

--- shortcut/urls.py
+++ shortcut/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path
 
 from mainapp.views import (index, shorturl, redirect_on_site, check_clicks,
-                           clicks_counter, report_wrong_url, terms_of_service)
+                           clicks_counter, report_wrong_url, terms_of_service,
+                           contact)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,5 +13,6 @@ urlpatterns = [
     path('url-click-counter/<slug:short_url>', clicks_counter, name='clicks'),
     path('report-wrong-url/', report_wrong_url, name='report'),
     path('terms-of-service/', terms_of_service, name='terms'),
+    path('contact/', contact, name='contact'),
     path('<slug:short_url>/', redirect_on_site, name='redirect_on_site'),
 ]
