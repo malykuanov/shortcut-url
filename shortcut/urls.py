dd8aa900508a9c11a1565d7b33296162655a86cd
Mikhail Lykuanov <malykuanov@mail.ru>
2021-10-31 15:52:12 +0300

    Replace all routes view on CBV

--- shortcut/urls.py
+++ shortcut/urls.py
@@ -1,18 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 
-from mainapp.views import (index, shorturl, redirect_on_site, check_clicks,
-                           clicks_counter, report_wrong_url, terms_of_service,
-                           contact)
+from mainapp.views import (CheckClicks, Contact, TermsOfService,
+                           RedirectOnSite, ShortUrl, ClicksCounter, HomePage)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='home'),
-    path('shorturl/', shorturl, name='shorturl'),
-    path('url-click-counter/', check_clicks, name='check_clicks'),
-    path('url-click-counter/<slug:short_url>', clicks_counter, name='clicks'),
-    path('report-wrong-url/', report_wrong_url, name='report'),
-    path('terms-of-service/', terms_of_service, name='terms'),
-    path('contact/', contact, name='contact'),
-    path('<slug:short_url>/', redirect_on_site, name='redirect_on_site'),
+    path('', HomePage.as_view(), name='home'),
+    path('shorturl/', ShortUrl.as_view(), name='shorturl'),
+    path('url-click-counter/', CheckClicks.as_view(), name='check_clicks'),
+    path('url-click-counter/<slug:short_url>', ClicksCounter.as_view(), name='clicks'),
+    path('report-wrong-url/', Contact.as_view(), name='report'),
+    path('terms-of-service/', TermsOfService.as_view(), name='terms'),
+    path('contact/', Contact.as_view(), name='contact'),
+    path('<slug:short_url>/', RedirectOnSite.as_view(), name='redirect_on_site'),
 ]
