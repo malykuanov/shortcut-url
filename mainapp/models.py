d385354602fc818210f54a6ccff358338d6f1977
Mikhail Lykuanov <malykuanov@mail.ru>
2021-11-01 19:37:35 +0300

    Add blank parameters for short url (for create in API)

--- mainapp/models.py
+++ mainapp/models.py
@@ -6,7 +6,7 @@ from hashids import Hashids
 
 class Urls(models.Model):
     long_url = models.URLField(max_length=2000)
-    short_url = models.CharField(max_length=5)
+    short_url = models.CharField(max_length=5, blank=True)
     clicks = models.IntegerField(default=0)
     time_create = models.DateTimeField(auto_now_add=True)
 
