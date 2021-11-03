1fc1268631cb7db5aa9c71f46aaa1aed63036cff
Mikhail Lykuanov <malykuanov@mail.ru>
2021-11-03 21:38:01 +0300

    Add name for path

--- mainapp/api/urls.py
+++ mainapp/api/urls.py
@@ -6,6 +6,6 @@ from .views import UrlsListView, UrlDetailView
 app_name = "mainapp"
 
 urlpatterns = [
-    path('urls/', UrlsListView.as_view()),
-    path('urls/<slug:short_url>', UrlDetailView.as_view()),
+    path('urls/', UrlsListView.as_view(), name='urls'),
+    path('urls/<slug:short_url>', UrlDetailView.as_view(), name='url'),
 ]
