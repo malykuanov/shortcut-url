f1a352edafe8587d118ab73e64c72ad89c765a39
Mikhail Lykuanov <malykuanov@mail.ru>
2021-11-05 20:55:46 +0300

    isort

--- mainapp/tests/test_api.py
+++ mainapp/tests/test_api.py
@@ -1,11 +1,10 @@
 import base64
 
 from django.contrib.auth.models import User
-from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APITestCase
 
 from mainapp.api.serializers import UrlsSerializer
 from mainapp.models import Urls
