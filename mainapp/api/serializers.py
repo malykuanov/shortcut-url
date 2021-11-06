ff2d8224111f3e7d77c9f63829760ea03fcb5c8a
Mikhail Lykuanov <malykuanov@mail.ru>
2021-11-06 14:56:11 +0300

    Changing the save method to create

--- mainapp/api/serializers.py
+++ mainapp/api/serializers.py
@@ -18,8 +18,9 @@ class CreateUrlSerializer(serializers.ModelSerializer):
         model = Urls
         fields = ('long_url', 'short_url', 'clicks', 'time_create')
 
-    def save(self):
-        long_url = self.validated_data.get('long_url')
-        clicks = self.validated_data.get('clicks', 0)
+    def create(self, validated_data):
+        long_url = validated_data.get('long_url')
+        clicks = validated_data.get('clicks', 0)
         url = Urls(long_url=long_url, clicks=clicks)
         url.save()
+        return url
