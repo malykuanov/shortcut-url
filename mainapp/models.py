4bfbe2c85617042c55a3269cc90fb88367d0e849
Mikhail Lykuanov <malykuanov@mail.ru>
2021-11-09 22:35:01 +0300

    Add field for owner short url

--- mainapp/models.py
+++ mainapp/models.py
@@ -7,6 +7,7 @@ class Urls(models.Model):
     long_url = models.URLField(max_length=2000)
     short_url = models.CharField(max_length=5, blank=True)
     clicks = models.PositiveIntegerField(default=0)
+    owner = models.CharField(max_length=200, default='AnonymousUser')
     time_create = models.DateTimeField(auto_now_add=True)
 
     class Meta:
