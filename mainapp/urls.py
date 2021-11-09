0a63a01a3853708a17e68a0bc06859950e95a4d1
Mikhail Lykuanov <malykuanov@mail.ru>
2021-11-09 20:58:35 +0300

    Add path for sign-in/up and logout user

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from .views import (CheckClicks, ClicksCounter, Contact, HomePage,
-                    RedirectOnSite, ShortUrl, TermsOfService)
+from .views import (logout_user, CheckClicks, ClicksCounter, Contact, HomePage,
+                    LoginUser, RedirectOnSite, RegistrationUser, ShortUrl,
+                    TermsOfService)
+
 
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
+    path('sign-in/', LoginUser.as_view(), name='sign-in'),
+    path('sign-up/', RegistrationUser.as_view(), name='sign-up'),
+    path('logout/', logout_user, name='logout'),
     path('shorturl/', ShortUrl.as_view(), name='shorturl'),
     path('url-click-counter/', CheckClicks.as_view(), name='check_clicks'),
     path('url-click-counter/<slug:short_url>',
