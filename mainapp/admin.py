2448eda2422331bcfddb6d89e8c56b721aaace9c
Mikhail Lykuanov <malykuanov@mail.ru>
2021-11-14 19:03:23 +0300

    Customize admin panel

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 
 from mainapp.models import Urls
 
-admin.site.register(Urls)
+
+class UrlsAdmin(admin.ModelAdmin):
+    list_display = (
+        'long_url_truncate', 'short_url', 'clicks',
+        'owner', 'time_create', 'time_last_click'
+    )
+    list_display_links = ('long_url_truncate', 'short_url')
+    search_fields = ('long_url', 'short_url')
+    readonly_fields = [
+        'long_url', 'short_url', 'clicks',
+        'owner', 'time_create', 'time_last_click'
+    ]
+
+
+admin.site.register(Urls, UrlsAdmin)
