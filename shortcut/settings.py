6ee723b7e7bdf6ef8ec8ce9535458f29c26868e1
Mikhail Lykuanov <malykuanov@mail.ru>
2021-11-14 17:45:04 +0300

    Fix correct load debug value from .env

--- shortcut/settings.py
+++ shortcut/settings.py
@@ -11,7 +11,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 HASHID_FIELD_SALT = env('HASHID_FIELD_SALT')
 
-DEBUG = env('DEBUG')
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = env('DJANGO_ALLOWED_HOSTS').split(" ")
 
